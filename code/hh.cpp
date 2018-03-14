ae7e3fe32ceea64bf965b8c74b9900b9f5fd7199
WatchingBoss <opil94@yandex.ru>
2018-03-14 16:27:36 +0300

    17 day

--- code/hh.cpp
+++ code/hh.cpp
@@ -55,6 +55,9 @@ GameUpdateAndRender(game_memory *Memory,
 					game_input *Input, game_offscreen_buffer *Buffer,
 					game_sound_output_buffer *SoundBuffer)
 {
+	Assert((&Input->Controllers[0].Terminator - &Input->Controllers[0].Buttons[0]) ==
+		   (ArrayCount(Input->Controllers[0].Buttons)));
+	
 	Assert(sizeof(game_state) <= Memory->PermanentStorageSize);
    	game_state *GameState = (game_state *)Memory->PermanentStorage;
 	if (!Memory->IsInitialized)
@@ -76,25 +79,38 @@ GameUpdateAndRender(game_memory *Memory,
 		Memory->IsInitialized = true;
 	}
 	
-	game_controller_input *Input0 = &Input->Controllers[0];
-	if(Input0->IsAnalog)
+	for(int ControllerIndex = 0;
+		ControllerIndex < ArrayCount(Input->Controllers);
+		++ControllerIndex)
 	{
-		// NOTE(santa): Use analog movement tuning
-		GameState->BlueOffset += (int)(4.0f * Input0->EndX);
-		GameState->ToneHz = 256 + (int)(128.0f * (Input0->EndY));
-	}
-	else
-	{
-		// NOTE(santa): Use digital movement tuning
-	}
+		game_controller_input *Controller = GetController(Input, ControllerIndex);
+		if(Controller->IsAnalog)
+		{
+			// NOTE(santa): Use analog movement tuning
+			GameState->BlueOffset += (int)(4.0f * Controller->StickAverageX);
+			GameState->ToneHz = 256 + (int)(128.0f * Controller->StickAverageY);
+		}
+		else
+		{
+			// NOTE(santa): Use digital movement tuning
+			if(Controller->MoveLeft.EndedDown)
+			{
+				GameState->BlueOffset -= 1;
+			}
+			
+			if(Controller->MoveRight.EndedDown)
+			{
+				GameState->BlueOffset += 1;
+			}
+		}
 
-	// Input.AButtonEndedDown;
-	// Input.AButtonHalfTransitionCount;
-	if (Input0->Down.EndedDown)
-	{
-		GameState->GreenOffset += 1;
-	}
-	
+		// Input.AButtonEndedDown;
+		// Input.AButtonHalfTransitionCount;
+		if (Controller->ActionDown.EndedDown)
+		{
+			GameState->GreenOffset += 1;
+		}
+	}	
 	// TODO(santa): Allow sampe offsets here for more robust platform options
 	GameOutputSound(SoundBuffer, GameState->ToneHz);
 	RenderWeirdGradient(Buffer, GameState->BlueOffset, GameState->GreenOffset);
---
+++ code/res/17_video/hh.h
@@ -0,0 +1,166 @@
+#if !defined(HH_H)
+/* ===========================================================
+	$File: $
+	$Date: $
+	$Revision: $
+	$Creator: Egor Smirnov (Santa) $
+	$Notice: $
+   ===========================================================*/
+
+/*
+	NOTE(santa):
+
+	HH_INTERNAL:
+		0 - Build for public release
+		1 - Build for developer only
+
+	HH_SLOW:
+		0 - Not slow code allowed!
+		1 - Slow code welcome.
+ */
+
+#if HH_SLOW
+#define Assert(Expression) if(!(Expression)) {*(int *)0 = 0;}
+#else
+#define Assert(Expression)
+#endif
+
+#define Kilobytes(Value) ((Value) * 1024LL)
+#define Megabytes(Value) (Kilobytes(Value) * 1024LL)
+#define Gigabytes(Value) (Megabytes(Value) * 1024LL)
+#define Terabytes(Value) (Gigabytes(Value) * 1024LL)
+
+#define ArrayCount(Array) (sizeof(Array) / sizeof((Array)[0]))
+//TODO(santa): swap, min, max ... macros?
+
+inline uint32
+SafeTruncateUInt64(uint64 Value)
+{
+	// TODO(santa): Desines for maximum valuesx
+	Assert(Value <= 0xFFFFFFFF);
+	uint32 Result = (uint32)Value;
+	return(Result);
+}
+
+/*
+	NOTE(santa): Services that the platform layer provides to the game
+*/
+
+#if HH_INTERNAL
+/*
+IMPORTANT(santa):
+
+These are NOT for doint anything in the shipping game - they are
+blocking and the write doesn't protect against lost data! 
+*/
+struct debug_read_file_result
+{
+	uint32 ContentsSize;
+	void *Contents;
+};
+internal debug_read_file_result DEBUGPlatformReadEntireFile(char *Filename);
+internal void DEBUGPlatformFreeFileMemory(void *Memory);
+internal bool32 DEBUGPlatformWrittenEntireFile(char *Filename, uint32 MemorySize, void *Memory);
+#endif
+
+/*
+	NOTE(santa): Services that the game provides to the platform layer.
+	(this may expand in the future - sound on separate thread, etc.)
+*/
+
+// FOUR THINGS - timing, controller/keyboard input, bitmap buffer to use, sound buffer to use
+
+struct game_offscreen_buffer
+{
+	// NOTE(santa): Pixels are alwasy 32-bits wide, Memory Order BB GG RR XX
+	void *Memory;
+	int Width;
+	int Height;
+	int Pitch;
+};
+
+struct game_sound_output_buffer
+{
+	int SamplesPerSecond;
+	int SampleCount;
+	int16 *Samples;
+};
+
+struct game_button_state
+{
+	int HalfTransitionCount;
+	bool32 EndedDown;
+};
+
+struct game_controller_input
+{
+	bool32 IsConnected;
+	bool32 IsAnalog;
+ 	real32 StickAverageX;
+	real32 StickAverageY;
+
+	union
+	{
+		game_button_state Buttons[12];
+		struct
+		{
+			game_button_state MoveUp;
+			game_button_state MoveDown;
+			game_button_state MoveLeft;
+			game_button_state MoveRight;
+
+			game_button_state ActionUp;
+			game_button_state ActionDown;
+			game_button_state ActionLeft;
+			game_button_state ActionRight;
+
+			game_button_state LeftShoulder;
+			game_button_state RightShoulder;
+
+			game_button_state Start;
+			game_button_state Back;
+
+			// NOTE(santa): All buttons must be added above this line
+			game_button_state Terminator;
+		};
+	};
+};
+
+struct game_input
+{
+	game_controller_input Controllers[5];
+};
+inline game_controller_input *GetController(game_input *Input, int unsigned ControllerIndex)
+{
+	Assert(ControllerIndex < ArrayCount(Input->Controllers));
+
+	game_controller_input *Result = &Input->Controllers[ControllerIndex];
+	return(Result);
+}
+
+struct game_memory
+{
+	bool32 IsInitialized;
+	uint64 PermanentStorageSize;
+	void *PermanentStorage; // NOTE(santa): REQUIRED to be cleared to zero at startup
+	uint64 TransientStorageSize;
+	void *TransientStorage; // NOTE(santa): REQUIRED to be cleared to zero at startup
+};
+
+internal void GameUpdateAndRender(game_memory *Memory,
+								  game_input *Input, game_offscreen_buffer *Buffer,
+								  game_sound_output_buffer *SoundBuffer);
+
+//
+//
+//
+
+struct game_state
+{
+	int ToneHz;
+	int GreenOffset;
+	int BlueOffset;
+};
+
+#define HH_H
+#endif
