20a4ab94c9b1d40d93501c4547911e0e2dc77276
WatchingBoss <opil94@yandex.ru>
2018-03-27 12:11:57 +0300

    Changes

--- misc/shell.bat
+++ misc/shell.bat
@@ -1,4 +1,4 @@
 @echo off
 subst f:\documents\projects_c\RHH
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-set path=f:\documents\projects_c\RHH\misc;%path%
+set path=f:\documents\projects_c\RHH\RHH\misc;%path%
