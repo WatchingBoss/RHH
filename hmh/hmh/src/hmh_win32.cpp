4451cb996a18c426f337417fa050c33f53b91bce
Watchingboss <opil94@yandex.ru>
2018-11-29 15:15:31 +0300

    DAY 1: Opening WIN32 Window

--- hmh/hmh/src/hmh_win32.cpp
+++ hmh/hmh/src/hmh_win32.cpp
@@ -2,8 +2,59 @@
 
 #include "hmh_win32.h"
 
+constexpr char *WindowClassName = "HandmadeHeroWindowClass";
+
+LRESULT CALLBACK MainWindowCallback( HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam ) {
+	LRESULT result = 0;
+
+	switch ( msg ) {
+		case WM_SIZE: OutputDebugStringA( "SIZE\n" ); break;
+		case WM_CLOSE: DestroyWindow( hwnd ); break;
+		case WM_DESTROY: PostQuitMessage( 0 ); break;
+		case WM_ACTIVATEAPP: OutputDebugStringA( "ACTIVATEAPP\n" ); break;
+		case WM_PAINT: {
+			PAINTSTRUCT Paint;
+			HDC         dc = BeginPaint( hwnd, &Paint );
+
+			int X      = Paint.rcPaint.left;
+			int Y      = Paint.rcPaint.top;
+			int width  = Paint.rcPaint.right - X;
+			int height = Paint.rcPaint.bottom - Y;
+
+			static DWORD op = WHITENESS;
+			PatBlt( dc, X, Y, width, height, op );
+
+			op == WHITENESS ? op = BLACKNESS : op = WHITENESS;
+
+			EndPaint( hwnd, &Paint );
+		}
+		default: result = DefWindowProc( hwnd, msg, wParam, lParam );
+	}
+	return result;
+}
+
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, int nCmdShow ) {
-	MessageBox( NULL, "This works fine", "Title", MB_OK );
+	WNDCLASS WindowClass      = {};
+	WindowClass.style         = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+	WindowClass.lpfnWndProc   = MainWindowCallback;
+	WindowClass.hInstance     = hInstance;
+	WindowClass.lpszClassName = WindowClassName;
+
+	if ( RegisterClass( &WindowClass ) ) {
+		HWND WindowHandle = CreateWindowEx( NULL, WindowClassName, "Like handmade hero",
+		                                    WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+		                                    CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		                                    CW_USEDEFAULT, NULL, NULL, hInstance, NULL );
+		if ( WindowHandle ) {
+			MSG Message = {};
+			while ( GetMessage( &Message, NULL, 0, 0 ) > 0 ) {
+				TranslateMessage( &Message );
+				DispatchMessage( &Message );
+			}
+		} else
+			MessageBox( NULL, "CreateWindowEx", "Error", MB_OK );
+	} else
+		MessageBox( NULL, "RegisterClass", "Error", MB_OK );
 
 	return 0;
 }
